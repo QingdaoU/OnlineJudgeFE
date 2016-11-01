165d3a6a2d37e8f915579f101d043a6a53fec78b
tw1997 <t888y@qq.com>
2016-11-01 22:53:35 +0800

    添加Simditor组件 修复了分页和筛选的bug

---
+++ src/components/Simditor.vue
@@ -0,0 +1,37 @@
+<template>
+  <textarea id="editor" autofocus></textarea>
+</template>
+
+<script>
+import Simditor from 'simditor'
+import 'simditor/styles/simditor.css'
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: '这里输入文字...'
+    },
+    toolbar: {
+      type: Array,
+      default: () => ['title', 'bold', 'italic', 'underline', 'fontScale', 'color', 'ol', 'ul', '|', 'link', 'image', 'hr', '|', 'indent', 'outdent', 'alignment']
+    }
+  },
+  data () {
+    return {
+      editor: null
+    }
+  },
+  mounted () {
+    Simditor.locale = 'zh-CN'
+    this.editor = new Simditor({
+      textarea: document.getElementById('editor'),
+      placeholder: this.placeholder,
+      toolbar: this.toolbar,
+      pasteImage: true
+    })
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
