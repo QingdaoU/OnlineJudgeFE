cf23108d3dd4c9ae2e3bcc9674a7554728574ce8
tw <t888y@qq.com>
2017-01-25 00:02:18 +0800

    use moment.js

--- src/filters.js
+++ src/filters.js
@@ -1,20 +1,7 @@
+import moment from 'moment'
 // 友好显示时间
-export function timeAgo (time) {
-  const between = Date.now() / 1000 - Number(time)
-  if (between < 3600) {
-    return pluralize(~~(between / 60), ' minute')
-  } else if (between < 86400) {
-    return pluralize(~~(between / 3600), ' hour')
-  } else {
-    return pluralize(~~(between / 86400), ' day')
-  }
-}
-
-function pluralize (time, label) {
-  if (time === 1) {
-    return time + label
-  }
-  return time + label + 's'
+export function fromNow (time) {
+  return moment(time).fromNow()
 }
 
 // 只显示日期
