f0d65c156578ba7949b43a44c8300e8925b7d0af
zemal <rawidn@163.com>
2017-06-22 19:58:14 +0800

    改写navbar

--- oj/src/App.vue
+++ oj/src/App.vue
@@ -1,77 +1,77 @@
 <template>
-<div>
   <div>
     <Navbar></Navbar>
-  </div>
-  <div class="content-app">
-    <router-view></router-view>
-    <div class="footer">
-      Build Version: {{ version }}
+    <div class="content-app">
+      <router-view></router-view>
+      <div class="footer">
+        Build Version: {{ version }}
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
-import 'font-awesome/css/font-awesome.min.css'
-import Navbar from './components/Navbar.vue'
-// import api from './api.js'
+  import 'font-awesome/css/font-awesome.min.css'
+  import Navbar from './components/Navbar.vue'
+  // import api from './api.js'
 
-export default {
-  name: 'app',
-  data() {
-    return {
-      version: process.env.VERSION
-    }
-  },
-  components: {
-    Navbar
-  },
-  methods: {}
-}
+  export default {
+    name: 'app',
+    data() {
+      return {
+        version: process.env.VERSION
+      }
+    },
+    components: {
+      Navbar
+    },
+    methods: {}
+  }
 </script>
 
 <style lang="less">
-body {
-    margin: 0;
-    background-color: rgb(236, 242, 247)!important;
-}
 
-a {
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    background-color: rgb(236, 242, 247) !important;
+  }
+
+  a {
+    text-decoration: none;
     background-color: transparent;
     -webkit-text-decoration-skip: objects;
-}
-a:active,
-a:hover {
+  }
+
+  a:active,
+  a:hover {
     outline-width: 0;
-}
+  }
 
-img {
+  img {
     border-style: none;
-}
+  }
 
-body {
+  body {
     font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
     overflow: auto;
-    font-weight: 400;
     -webkit-font-smoothing: antialiased;
     background-color: #EDECEC;
     overflow-y: scroll;
-}
+  }
 
-* {
-    box-sizing: border-box;
-}
-
-.content-app {
+  .content-app {
     padding-top: 20px;
     padding-right: 10px;
     padding-left: 10px;
-}
+  }
 
-.footer {
+  .footer {
     margin: 15px;
     text-align: center;
     font-size: small;
-}
+  }
 </style>
--- oj/src/components/Navbar.vue
+++ oj/src/components/Navbar.vue
@@ -1,82 +1,103 @@
 <template>
-<el-menu theme="dark" class="el-menu-demo" mode="horizontal" router>
-  <el-row :gutter="10">
-    <el-col :xs="2" :sm="2" :lg="2">&nbsp;</el-col>
-    <el-col :xs="2" :sm="2" :lg="2">
-      <div style="color:#bfcbd9;margin-top:16px;font-size:20px;">QDUOJ</div>
-    </el-col>
-    <el-col :xs="16" :sm="16" :lg="16">
-      <el-menu-item index="/problems">题目</el-menu-item>
-      <el-menu-item index="2">提交</el-menu-item>
-      <el-menu-item index="3">比赛</el-menu-item>
-      <el-menu-item index="4">排名</el-menu-item>
-      <el-menu-item index="5">帮助</el-menu-item>
-    </el-col>
-    <el-col :xs="3" :sm="3" :lg="3">
-      <template v-if="isLogin">
-        <el-submenu index="2">
-          <template slot="title">{{ info.user.username }}
-          </template>
-      <el-menu-item index="2-1" :route="{path:'/user/'+info.user.username}">我的主页</el-menu-item>
-      <el-menu-item index="2-2">我的提交</el-menu-item>
-      <el-menu-item index="/setting">设置</el-menu-item>
-      <el-menu-item index="/logout">退出</el-menu-item>
-      </el-submenu>
-      </template>
-      <template v-else>
-        <div class="">
-          <el-menu-item class="login-btn" index="/login">登录</el-menu-item>
-          <el-menu-item class="login-separator" index="/">/</el-menu-item>
-          <el-menu-item class="login-btn" index="/register">注册</el-menu-item>
-        </div>
-      </template>
-    </el-col>
-  </el-row>
-</el-menu>
+  <div class="nav">
+    <el-row :gutter="10" type="flex" justify="space-around">
+      <el-col :xs="1" :sm="1" :lg="1">&nbsp;</el-col>
+      <el-col :xs="2" :sm="2" :lg="2">
+        <div id="brand">QDUOJ</div>
+      </el-col>
+      <el-col :xs="16" :sm="16" :lg="16">
+        <el-menu theme="dark" mode="horizontal" router>
+          <el-menu-item index="/problems">Problems</el-menu-item>
+          <el-menu-item index="3">Contests</el-menu-item>
+          <el-menu-item index="2">Status</el-menu-item>
+          <el-menu-item index="4">Rank</el-menu-item>
+          <el-menu-item index="5">About</el-menu-item>
+        </el-menu>
+      </el-col>
+      <el-col :xs="3" :sm="3" :lg="3">
+        <template v-if="isLogin">
+          <el-menu theme="dark" mode="horizontal" router>
+            <el-submenu index="2">
+              <template slot="title">{{ info.user.username }}
+              </template>
+              <el-menu-item index="2-1" :route="{path:'/user/'+info.user.username}">Home</el-menu-item>
+              <el-menu-item index="2-2">Submissions</el-menu-item>
+              <el-menu-item index="/setting">Settings</el-menu-item>
+              <el-menu-item index="/logout">Logout</el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </template>
+        <template v-else>
+          <div class="">
+            <el-menu theme="dark" mode="horizontal" router>
+              <el-menu-item class="login-btn" index="/login">Login</el-menu-item>
+              <el-menu-item class="login-separator" index="/">/</el-menu-item>
+              <el-menu-item class="login-btn" index="/register">Register</el-menu-item>
+            </el-menu>
+          </div>
+        </template>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 
 <script>
-import api from '../api.js'
+  import api from '../api.js'
 
-export default {
-  data() {
-    return {
-      isLogin: false
-    }
-  },
-  mounted() {
-    this.getMyInfo()
-  },
-  watch: {
-    // 如果路由有变化，会再次执行该方法
-    '$route': 'getMyInfo'
-  },
-  methods: {
-    getMyInfo() {
-      api.getMyInfo().then(res => {
-        this.info = res.data.data
-        this.isLogin = true
-      }, res => {
-        this.isLogin = false
-      })
+  export default {
+    data() {
+      return {
+        isLogin: false
+      }
+    },
+    mounted() {
+      this.getMyInfo()
+    },
+    watch: {
+      // 如果路由有变化，会再次执行该方法
+      '$route': 'getMyInfo'
+    },
+    methods: {
+      getMyInfo() {
+        api.getMyInfo().then(res => {
+          this.info = res.data.data
+          this.isLogin = true
+        }, res => {
+          this.isLogin = false
+        })
+      }
     }
   }
-}
 </script>
 
-<style lang="css">
-.login a{
-  text-decoration: none;
-}
-.login-btn{
-  padding: 0!important;
-}
-.login-separator{
-  padding: 0 10px!important;
-  cursor: default!important;
-}
-.login-separator.is-active{
-  color: #bfcbd9!important;
-}
+<style lang="less" scoped>
+
+  .nav {
+    background-color: #324157;
+  }
+
+  .el-menu-item{
+    font-size: 15px;
+  }
+
+  #brand {
+    font-size: 23px;
+    display: inline-block;
+    line-height: 60px;
+    color: #E5E9F2;
+  }
+
+  .login-btn {
+    padding: 0 !important;
+  }
+
+  .login-separator {
+    padding: 0 10px !important;
+    cursor: default !important;
+  }
+
+  .login-separator.is-active {
+    color: #bfcbd9 !important;
+  }
 </style>
