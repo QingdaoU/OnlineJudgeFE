4a0b7a4bc21593e4f2b2d9f69bf6fed8caad6b8e
zema1 <rawidn@163.com>
2017-12-20 19:59:49 +0800

    update travis ci

--- deploy/sentry_release.sh
+++ deploy/sentry_release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+
 DATE=`date +%Y%m%d`
 COMMIT=`git rev-parse HEAD`
 VERSION="$DATE-${COMMIT:0:5}"
-TOKEN=95c95811dcf14d4e9fdf2e63b9e027367b3bd1db91ab42a49e72648b7751ae6d
 
 echo "Current version is $VERSION"
 
@@ -10,9 +10,9 @@ if [ ! -z $USE_SENTRY ] && [ $USE_SENTRY == '1' ]; then
 
 # create new release according to `VERSION`
 docker run --rm -it -v $(pwd):/work getsentry/sentry-cli \
-   sentry-cli --auth-token $TOKEN releases -o onlinejudge -p onlinejudgefe new $VERSION
+   sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases -o onlinejudge -p onlinejudgefe new $VERSION
 
 # upload js and source_maps
 docker run --rm -it -v $(pwd):/work getsentry/sentry-cli \
-   sentry-cli --auth-token $TOKEN releases -o onlinejudge -p onlinejudgefe files $VERSION upload-sourcemaps ./dist/static/js
+   sentry-cli --auth-token $SENTRY_AUTH_TOKEN releases -o onlinejudge -p onlinejudgefe files $VERSION upload-sourcemaps ./dist/static/js
 fi
