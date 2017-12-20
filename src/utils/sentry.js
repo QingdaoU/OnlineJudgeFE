d9136f584c1f6da046b029f410e7d75957d274f5
zema1 <rawidn@163.com>
2017-12-20 16:30:10 +0800

    使用travis上传发布sentry_release

--- src/utils/sentry.js
+++ src/utils/sentry.js
@@ -3,7 +3,9 @@ import Raven from 'raven-js'
 import RavenVue from 'raven-js/plugins/vue'
 
 Raven
-    .config('https://6234a51e61a743b089ed64c51d2f6ea9@sentry.io/258234')
+    .config('https://6234a51e61a743b089ed64c51d2f6ea9@sentry.io/258234', {
+      release: process.env.VERSION
+    })
     .addPlugin(RavenVue, Vue)
     .install()
 Raven.setUserContext({
