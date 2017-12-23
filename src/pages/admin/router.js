9a76e0933b091e55ed0555e134b506ce0b29df4b
zema1 <rawidn@163.com>
2017-12-23 16:51:39 +0800

    添加去除无用test_case的功能

--- src/pages/admin/router.js
+++ src/pages/admin/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 // 引入 view 组件
-import { Announcement, Conf, Contest, ContestList, Home, JudgeServer, Login, Problem, ProblemList, User } from './views'
-
+import { Announcement, Conf, Contest, ContestList, Home, JudgeServer, Login,
+  Problem, ProblemList, User, PruneTestCase } from './views'
 Vue.use(VueRouter)
 
 export default new VueRouter({
@@ -42,6 +42,11 @@ export default new VueRouter({
           path: '/judge-server',
           name: 'judge-server',
           component: JudgeServer
+        },
+        {
+          path: '/prune-test-case',
+          name: 'prune-test-case',
+          component: PruneTestCase
         },
         {
           path: '/problems',
@@ -93,7 +98,6 @@ export default new VueRouter({
           name: 'edit-contest-problem',
           component: Problem
         }
-
       ]
     },
     {
---
+++ src/pages/admin/views/general/PruneTestCase.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <panel>
+      <span slot="title">Prune Test Case
+        <el-popover placement="right" trigger="hover">
+          These test cases are not owned by any problem, you can clean them safely.
+          <i slot="reference" class="el-icon-fa-question-circle import-user-icon"></i>
+        </el-popover>
+      </span>
+      <el-table :data="data">
+        <el-table-column
+          label="Create Time">
+          <template slot-scope="{row}">
+            {{row.create_time | timestampFormat }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="id"
+          label="Test Case ID">
+        </el-table-column>
+        <el-table-column
+          label="Option"
+          fixed="right"
+          width="200">
+          <template slot-scope="{row}">
+            <icon-btn name="Delete" icon="trash" @click.native="deleteTestCase(row.id)"></icon-btn>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="panel-options" v-show="data.length > 0">
+        <el-button type="warning" size="small"
+                   :loading="loading"
+                   icon="el-icon-fa-trash"
+                   @click="deleteTestCase()">Delete All
+        </el-button>
+      </div>
+    </panel>
+  </div>
+</template>
+
+<script>
+  import api from '@admin/api'
+  import moment from 'moment'
+
+  export default {
+    name: 'prune-test-case',
+    data () {
+      return {
+        data: [],
+        loading: false
+      }
+    },
+    mounted () {
+      this.init()
+    },
+    methods: {
+      init () {
+        api.getInvalidTestCaseList().then(resp => {
+          this.data = resp.data.data
+        }, () => {
+        })
+      },
+      deleteTestCase (id) {
+        if (!id) {
+          this.loading = true
+        }
+        api.pruneTestCase(id).then(resp => {
+          this.loading = false
+          this.init()
+        })
+      }
+    },
+    filters: {
+      timestampFormat (value) {
+        return moment.unix(value).format('YYYY-M-D  HH:mm:ss')
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
