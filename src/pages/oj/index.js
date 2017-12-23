c81d8b10dc5e2873d204ce5832efe33f91bd425a
zema1 <rawidn@163.com>
2017-12-23 18:19:45 +0800

    修复submission detail在部分浏览器下不显示代码的问题

--- src/pages/oj/index.js
+++ src/pages/oj/index.js
@@ -48,8 +48,7 @@ Vue.directive('highlight', {
   deep: true,
   bind: function (el, binding) {
     // on first bind, highlight all targets
-    let targets = el.querySelectorAll('code')
-    targets.forEach((target) => {
+    Array.from(el.querySelectorAll('code')).forEach((target) => {
       // if a value is directly assigned to the directive, use this
       // instead of the element content.
       if (binding.value) {
@@ -60,12 +59,11 @@ Vue.directive('highlight', {
   },
   componentUpdated: function (el, binding) {
     // after an update, re-fill the content and then highlight
-    let targets = el.querySelectorAll('code')
-    targets.forEach((target) => {
+    Array.from(el.querySelectorAll('code')).forEach((target) => {
       if (binding.value) {
         target.textContent = binding.value
-        hljs.highlightBlock(target)
       }
+      hljs.highlightBlock(target)
     })
   }
 })
