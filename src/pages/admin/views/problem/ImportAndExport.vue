b98ce7350a826fc5d8e1bc18e189e7ee8a448ed6
virusdefender <virusdefender@outlook.com>
2018-10-14 21:11:28 +0800

    fix import fps url

--- src/pages/admin/views/problem/ImportAndExport.vue
+++ src/pages/admin/views/problem/ImportAndExport.vue
@@ -77,7 +77,7 @@
     <panel title="Import FPS Problems (beta)">
       <el-upload
         ref="FPS"
-        action="/api/admin/import_problem"
+        action="/api/admin/import_fps"
         name="file"
         :file-list="fileList2"
         :show-file-list="true"
