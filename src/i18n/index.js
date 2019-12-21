02de04feb5e3444d8d2b1901706755eabc7366e8
shaohuihuang <hsh@xmu.edu.cn>
2019-12-21 07:09:06 +0000

    增加iview组件的国际化支持

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
+import enUS from 'iview/dist/locale/en-US'
+import zhCN from 'iview/dist/locale/zh-CN'
+import zhTW from 'iview/dist/locale/zh-TW'
 
 Vue.use(VueI18n)
 
 const languages = [
-  {value: 'en-US', label: 'English'},
-  {value: 'zh-CN', label: '简体中文'},
-  {value: 'zh-TW', label: '繁體中文'}
+  {value: 'en-US', label: 'English', ui: enUS},
+  {value: 'zh-CN', label: '简体中文', ui: zhCN},
+  {value: 'zh-TW', label: '繁體中文', ui: zhTW}
 ]
 const messages = {}
 
@@ -15,7 +18,7 @@ for (let lang of languages) {
   let locale = lang.value
   let m = require(`./oj/${locale}`).m
   Object.assign(m, require(`./admin/${locale}`).m)
-  messages[locale] = {m: m}
+  messages[locale] = Object.assign({m: m}, lang.ui)
 }
 // load language packages
 export default new VueI18n({
