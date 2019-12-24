022faaf3a0e5dfe1870091875f3f5f0154af0571
shaohuihuang <hsh@xmu.edu.cn>
2019-12-24 07:15:14 +0000

    解决了后台管理Element UI的国际化问题

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import enUS from 'iview/dist/locale/en-US'
-import zhCN from 'iview/dist/locale/zh-CN'
-import zhTW from 'iview/dist/locale/zh-TW'
+import ivenUS from 'iview/dist/locale/en-US'
+import ivzhCN from 'iview/dist/locale/zh-CN'
+import ivzhTW from 'iview/dist/locale/zh-TW'
+import elenUS from 'element-ui/lib/locale/lang/en'
+import elzhCN from 'element-ui/lib/locale/lang/zh-CN'
+import elzhTW from 'element-ui/lib/locale/lang/zh-TW'
 
 Vue.use(VueI18n)
 
 const languages = [
-  {value: 'en-US', label: 'English', ui: enUS},
-  {value: 'zh-CN', label: '简体中文', ui: zhCN},
-  {value: 'zh-TW', label: '繁體中文', ui: zhTW}
+  {value: 'en-US', label: 'English', iv: ivenUS, el: elenUS},
+  {value: 'zh-CN', label: '简体中文', iv: ivzhCN, el: elzhCN},
+  {value: 'zh-TW', label: '繁體中文', iv: ivzhTW, el: elzhTW}
 ]
 const messages = {}
 
@@ -18,7 +21,8 @@ for (let lang of languages) {
   let locale = lang.value
   let m = require(`./oj/${locale}`).m
   Object.assign(m, require(`./admin/${locale}`).m)
-  messages[locale] = Object.assign({m: m}, lang.ui)
+  let ui = Object.assign(lang.iv, lang.el)
+  messages[locale] = Object.assign({m: m}, ui)
 }
 // load language packages
 export default new VueI18n({
