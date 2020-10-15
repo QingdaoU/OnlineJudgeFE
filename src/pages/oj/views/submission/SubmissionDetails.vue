5397ffa83f66621bbc69c862096b7069a9e76872
shaohuihuang <hsh@xmu.edu.cn>
2020-10-15 00:59:16 +0000

    注释掉编译失败时调用谷歌翻译的功能

--- src/pages/oj/views/submission/SubmissionDetails.vue
+++ src/pages/oj/views/submission/SubmissionDetails.vue
@@ -6,6 +6,7 @@
         <span class="title" v-if="isCE">[main.c:后面的两个数字分别表示错误代码所在的“行号”和“列号”]</span>
         <div slot="desc" class="content">
           <template v-if="isCE">
+            <!--
             请选择出错信息的语言：
             <i-switch size="large" v-model="isCn">
                 <span slot="open">中文</span>
@@ -13,6 +14,8 @@
             </i-switch>
             <pre v-if="isCn">{{submission.statistic_info.err_info_cn}}</pre>
             <pre v-else>{{submission.statistic_info.err_info}}</pre>
+            -->
+            <pre>{{submission.statistic_info.err_info}}</pre>
           </template>
           <template v-else>
             <span>{{$t('m.Time')}}: {{submission.statistic_info.time_cost | submissionTime}}</span>
