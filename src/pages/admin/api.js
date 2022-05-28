b80d35cf577e459cc8ad9856cdd9fd715cf11d3d
Duong Vo <duongvox99@gmail.com>
2022-05-28 14:34:42 +0700

    refactor: reuse ajax function

--- src/pages/admin/api.js
+++ src/pages/admin/api.js
@@ -1,12 +1,5 @@
-import Vue from 'vue'
-import router from './router'
-import axios from 'axios'
 import utils from '@/utils/utils'
-
-Vue.prototype.$http = axios
-axios.defaults.baseURL = '/api'
-axios.defaults.xsrfHeaderName = 'X-CSRFToken'
-axios.defaults.xsrfCookieName = 'csrftoken'
+import {ajax} from '../oj/api'
 
 export default {
   // 登录
@@ -297,46 +290,4 @@ export default {
       data
     })
   }
-}
-
-/**
- * @param url
- * @param method get|post|put|delete...
- * @param params like queryString. if a url is index?a=1&b=2, params = {a: '1', b: '2'}
- * @param data post data, use for method put|post
- * @returns {Promise}
- */
-function ajax (url, method, options) {
-  if (options !== undefined) {
-    var {params = {}, data = {}} = options
-  } else {
-    params = data = {}
-  }
-  return new Promise((resolve, reject) => {
-    axios({
-      url,
-      method,
-      params,
-      data
-    }).then(res => {
-      // API正常返回(status=20x), 是否错误通过有无error判断
-      if (res.data.error !== null) {
-        Vue.prototype.$error(res.data.data)
-        reject(res)
-        // // 若后端返回为登录，则为session失效，应退出当前登录用户
-        if (res.data.data.startsWith('Please login')) {
-          router.push({name: 'login'})
-        }
-      } else {
-        resolve(res)
-        if (method !== 'get') {
-          Vue.prototype.$success('Succeeded')
-        }
-      }
-    }, res => {
-      // API请求异常，一般为Server error 或 network error
-      reject(res)
-      Vue.prototype.$error(res.data.data)
-    })
-  })
 }
--- src/pages/oj/api.js
+++ src/pages/oj/api.js
@@ -6,6 +6,9 @@ Vue.prototype.$http = axios
 axios.defaults.baseURL = '/api'
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.validateStatus = function (status) {
+  return status === 200 || status === 400 || status === 401
+}
 
 export default {
   getWebsiteConf (params) {
@@ -278,7 +281,7 @@ export default {
  * @param data post data, use for method put|post
  * @returns {Promise}
  */
-function ajax (url, method, options) {
+export function ajax (url, method, options) {
   if (options !== undefined) {
     var {params = {}, data = {}} = options
   } else {
@@ -291,24 +294,23 @@ function ajax (url, method, options) {
       params,
       data
     }).then(res => {
-      // API正常返回(status=20x), 是否错误通过有无error判断
       if (res.data.error !== null) {
         Vue.prototype.$error(res.data.data)
         reject(res)
-        // 若后端返回为登录，则为session失效，应退出当前登录用户
+
         if (res.data.data.startsWith('Please login')) {
           store.dispatch('changeModalStatus', {'mode': 'login', 'visible': true})
         }
       } else {
-        resolve(res)
         // if (method !== 'get') {
         //   Vue.prototype.$success('Succeeded')
         // }
+        resolve(res)
       }
-    }, res => {
-      // API请求异常，一般为Server error 或 network error
-      reject(res)
-      Vue.prototype.$error(res.data.data)
+    }, error => {
+      // API request exception, usually Server error or network error
+      Vue.prototype.$error(error.message)
+      reject(error)
     })
   })
 }
