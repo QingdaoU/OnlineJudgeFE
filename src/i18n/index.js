3694ff5eca5cf0ad0e73cf5b145672265e4db491
root2 <wjsrl528@naver.com>
2023-02-01 21:58:34 +0900

    Korean Patch

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,20 +1,13 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-// ivew UI
-import ivenUS from 'iview/dist/locale/en-US'
-import ivzhCN from 'iview/dist/locale/zh-CN'
-import ivzhTW from 'iview/dist/locale/zh-TW'
-// element UI
-import elenUS from 'element-ui/lib/locale/lang/en'
-import elzhCN from 'element-ui/lib/locale/lang/zh-CN'
-import elzhTW from 'element-ui/lib/locale/lang/zh-TW'
 
 Vue.use(VueI18n)
 
 const languages = [
-  {value: 'en-US', label: 'English', iv: ivenUS, el: elenUS},
-  {value: 'zh-CN', label: '简体中文', iv: ivzhCN, el: elzhCN},
-  {value: 'zh-TW', label: '繁體中文', iv: ivzhTW, el: elzhTW}
+  {value: 'en-US', label: 'English'},
+  {value: 'zh-CN', label: '简体中文'},
+  {value: 'zh-TW', label: '繁體中文'},
+  {value: 'ko-KO', label: '한국어'}
 ]
 const messages = {}
 
@@ -23,12 +16,11 @@ for (let lang of languages) {
   let locale = lang.value
   let m = require(`./oj/${locale}`).m
   Object.assign(m, require(`./admin/${locale}`).m)
-  let ui = Object.assign(lang.iv, lang.el)
-  messages[locale] = Object.assign({m: m}, ui)
+  messages[locale] = {m: m}
 }
 // load language packages
 export default new VueI18n({
-  locale: 'en-US',
+  locale: 'ko-KO',
   messages: messages
 })
 
--- src/pages/oj/App.vue
+++ src/pages/oj/App.vue
@@ -7,7 +7,7 @@
       </transition>
       <div class="footer">
         <p v-html="website.website_footer"></p>
-        <p>Powered by <a href="https://github.com/QingdaoU/OnlineJudge">OnlineJudge</a>
+        <p>Powered by <a href="https://github.com/minsuuuj/OnlineJudgeFE">UNTOC OnlineJudge</a>
           <span v-if="version">&nbsp; Version: {{ version }}</span>
         </p>
       </div>
